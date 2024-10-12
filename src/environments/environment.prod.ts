a287f7bd3c0236e1c453647981727c5bc0ab85f9
Erick Herrera Cabrera <erickmherrera01@gmail.com>
2024-10-12 13:05:45 -0600

    modified:   src/environments/environment.prod.ts 	modified:   src/main.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'https://6csl61rcc5.execute-api.us-east-1.amazonaws.com'
 };
